acf793a712407911e5a0761bb6081e4281f91e99
Mayank Kumar Prasad <mayank@perceptsystems.com>
2025-08-21 11:50:05 +0530

    satellite view

--- app/components/map.tsx
+++ app/components/map.tsx
@@ -94,9 +94,13 @@ const Map: React.FC = () => {
                 style={{ height: '100vh', width: '100%' }}
             >
                 <TileLayer
-                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                    url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+                    url="https://tiles.stadiamaps.com/tiles/alidade_satellite/{z}/{x}/{y}{r}.jpg"
+                    attribution='&copy; CNES, Distribution Airbus DS, © Airbus DS, © PlanetObserver (Contains Copernicus Data) | &copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    minZoom={0}
+                    maxZoom={20}
                 />
+
+
                 <MapEvents />
                 {notes.map((note) => (
                     <Marker key={note.id} position={[note.lat, note.lng]}>
